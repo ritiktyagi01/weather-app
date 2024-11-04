2c2bf6bda83db0edd2250cdb1f9ef733ac953226
ritiktyagi01 <ritiktyagi287@gmail.com>
2024-11-04 22:12:49 +0530

    fucker

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+const  App = () =>{
+  return (
+    <> 
+    <h1> welcome to my class </h1>
+   <button className="btn btn-success"> thapa technical</button> 
+    
+    </>
+
+
+  );
+};
+
+export default App;
